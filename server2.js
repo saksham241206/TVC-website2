6a0bab33eb096b93cd3c183fcc6060013fbc3f3b
Dungeon Master <makkarsaksham232@gmail.com>
2026-01-29 16:40:40 +0530

    add

---
+++ server2.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(__dirname, "index1.html");
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            res.writeHead(500);
+            res.end("Server Error");
+        } else {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(content);
+        }
+    });
+});
+
+server.listen(4000, () => {
+    console.log("Server running at http://localhost:4000");
+});
